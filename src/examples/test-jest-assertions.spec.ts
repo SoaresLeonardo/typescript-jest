04d5552414a5e14f209d7933c98e3e8fca66a996
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-26 10:51:33 -0300

    Exemplos

---
+++ src/examples/test-jest-assertions.spec.ts
@@ -0,0 +1,18 @@
+describe('Primitive values', () => {
+  it('should test jest assertions', () => {
+    const number = 10;
+
+    // expect(number).toBe(10);
+    // expect(number).toEqual(10);
+    // expect(number).not.toBeNull;
+
+    // Maior que
+    // expect(number).toBeGreaterThan(9);
+
+    // Maior ou igual a
+    // expect(number).toBeGreaterThanOrEqual(10);
+
+    // Menor que
+    // expect(number).toBeLessThan(11);
+  });
+});
