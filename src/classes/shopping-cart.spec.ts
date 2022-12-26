a261a8b5ee7804ec2a485a8d3b1c7aeef360dccb
Leonardo Henrique <100442262+SoaresLeonardo@users.noreply.github.com>
2022-12-26 10:48:11 -0300

    Shopping cart

---
+++ src/classes/shopping-cart.spec.ts
@@ -0,0 +1,35 @@
+import { Discount } from './discout';
+import { CartItem } from './interfaces/cart-item';
+import { ShoppingCart } from './shopping-cart';
+
+const createSut = () => {
+  const discountMock = createDiscountMock();
+  const sut = new ShoppingCart(discountMock);
+  return { sut, discountMock };
+};
+
+const createDiscountMock = () => {
+  class DiscountMock extends Discount {}
+  return new DiscountMock();
+};
+
+const createCartItem = (name: string, price: number) => {
+  class CartItemMock implements CartItem {
+    constructor(public name: string, public price: number) {}
+  }
+  return new CartItemMock(name, price);
+};
+
+describe('ShoppingCart', () => {
+  it('should be an empty cart when no product is added', () => {
+    const { sut } = createSut();
+    expect(sut.isEmpty()).toBe(true);
+  });
+
+  it('should have 2 cart items', () => {
+    const { sut } = createSut();
+    const cartItem1 = createCartItem('Camiseta', 40);
+    sut.addItem(cartItem1);
+    expect(sut.items.length).toBe(1);
+  });
+});
